739fd65c8191064a385893e75a56458762de8c6b
Alex Harvey <alexharv074@gmail.com>
2019-08-12 01:13:39 +1000

    Add puppet agent and provisioner config
    At this time however the agent fails to build, with error message seen:
    
      Error: Failed to generate an autosign token: Bolt: "bolt task run --nodes ssh://ec2-3-104-110-64.ap-southeast-2.compute.amazonaws.com -u root --no-host-key-check --format json --connect-timeout 120 autosign::generate_token certname=ip-172-31-8-92.ap-southeast-2.compute.internal": { "items": [{"node":"ssh://ec2-3-104-110-64.ap-southeast-2.compute.amazonaws.com","target":"ssh://ec2-3-104-110-64.ap-southeast-2.compute.amazonaws.com","action":null,"object":null,"status":"failure","result":{"_error":{"kind":"puppetlabs.tasks/connect-error","msg":"Authentication failed for user root@ec2-3-104-110-64.ap-southeast-2.compute.amazonaws.com","details":{},"issue_code":"AUTH_ERROR"}}}
      ],
      "node_count": 1, "elapsed_time": 0 }
      : exit status 2

--- main.tf
+++ main.tf
@@ -1,6 +1,25 @@
-resource "aws_instance" "puppet_master" {
-  ami           = "ami-08589eca6dcc9b39c"
+locals {
+  ami_id = "ami-08589eca6dcc9b39c"
+}
+
+resource "aws_instance" "master" {
+  ami           = local.ami_id
   instance_type = "t2.micro"
   key_name      = "default"
-  user_data     = templatefile("${path.module}/user-data.sh.tmpl", {})
+  user_data     = templatefile("${path.module}/user_data/master.sh.tmpl", {})
+}
+
+resource "aws_instance" "agent" {
+  ami           = local.ami_id
+  instance_type = "t2.micro"
+  key_name      = "default"
+  user_data     = templatefile("${path.module}/user_data/agent.sh.tmpl", {})
+
+  provisioner "puppet" {
+    server      = aws_instance.puppet_master.public_dns
+    connection {
+      host        = self.public_ip
+      private_key = file("${path.module}/keys/id_rsa")
+    }
+  }
 }
