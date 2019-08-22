e261b02c3ac285b58357f3a3afb820ff24c02550
Alex Harvey <alexharv074@gmail.com>
2019-08-22 20:29:07 +1000

    Refactor to use ec2-user instead of root
    Also use remote-exec provisioner to detect the end of user-data
    execution.

--- main.tf
+++ main.tf
@@ -7,20 +7,36 @@ resource "aws_instance" "master" {
   instance_type = "t2.micro"
   key_name      = "default"
   user_data     = file("${path.module}/user_data/master.sh")
+
+  connection {
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file("~/.ssh/default.pem")
+  }
+
+  provisioner "remote-exec" {
+    on_failure = continue
+    inline = [
+      "sudo sh -c 'while ! grep -q Cloud-init.*finished /var/log/cloud-init-output.log; do sleep 60; done'"
+    ]
+  }
 }
 
 resource "aws_instance" "agent" {
   ami           = local.ami_id
   instance_type = "t2.micro"
   key_name      = "default"
-  user_data     = file("${path.module}/user_data/agent.sh")
+
+  connection {
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file("~/.ssh/default.pem")
+  }
 
   provisioner "puppet" {
+    use_sudo    = true
     server      = aws_instance.master.public_dns
-    connection {
-      host        = self.public_ip
-      private_key = file("${path.module}/keys/id_rsa")
-    }
+    server_user = "ec2-user"
   }
 
   depends_on = [aws_instance.master]
--- user_data/master.sh
+++ user_data/master.sh
@@ -4,18 +4,6 @@
 #   couldn't find login name -- expanding `~'
 export HOME='/root'
 
-configure_ssh() {
-  sed -i '
-    s/.*PermitRootLogin.*/PermitRootLogin without-password/
-    ' /etc/ssh/sshd_config
-  systemctl restart sshd
-  mkdir -p -m 700 /root/.ssh
-  cat > /root/.ssh/authorized_keys <<'EOF'
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDZWI7KSYD2tR0k+oxUsoY/DCVzryyH1z2i01Bx+pVY1/hPNwgrLFKYJVGqQyOtXNn0UqLr5DRwii8OYZsqccs7LPSZPgIJzTQpHtqudPJ3eB4EhU0NNdjKcgvZcBPnJBu9tKHKo8pB7sEkcCcXIfjrQcOJk0Ljyqn1mRzT37Q7mouNWd4aQKq8Pdq+r9IYe4Rhi5Avgxd04lZ3hbojcTPxNyxL+6kQZ0plnt09drNOomirW2MkOeKN+ySNlYskhB/Xag/J6FVAcMMcKo3quVRvYEFi8Z/rt5AnO345ZLwVIGIGwcj3Tx7wAjEUdK/N9y7uv757pw0YMPrcNTZ+C/n5 puppet
-EOF
-  chmod 644 /root/.ssh/authorized_keys
-}
-
 install_puppetserver() {
   wget https://yum.puppet.com/puppet6-release-el-7.noarch.rpm
   rpm -Uvh puppet6-release-el-7.noarch.rpm
@@ -58,7 +46,6 @@ deploy_code() {
 }
 
 main() {
-  configure_ssh
   install_puppetserver
   configure_puppetserver
   configure_autosign
